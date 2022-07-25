64c60036a98cda410e71cbd74bddc986fdf766be
kwonheeyoung <heeyoung8748@gmail.com>
2022-07-25 15:29:10 +0900

    Feat: 제자리 점프 구현
    IEnumerator로 입력 시간을 받아 일정 높이 이내의 범위에서 점프하도록 했다.
    수정사항: 펭귄이 내려올 때 반동이 있는 것처럼 덜컹(?) 거리며 내려온다. 또한 나타난 오브젝트를 주시하고, 그 오브젝트를 주시한 채 해당 방향으로 앞/위 점프할 수 있도록 바꿔야 한다.

--- JumpJump/Assets/Scripts/PlayerMovement.cs
+++ JumpJump/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    public int MoveSpeed = 3;
-    public float _jumpdegree = 0.0f;
+    private float _buttonPressedTime = 0.0f;
+    private float _buttonReleasedTime = 0.0f;
+    public int MaxJumpDegree = 30;
+    private bool _isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +16,47 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
-    void Move()
-    {
-        float h = Input.GetAxis("Horizontal"); 
-        float v = Input.GetAxis("Vertical");
-    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _jumpdegree <= 250)
+        if(Input.GetKey(KeyCode.Space))
         {
-            _jumpdegree += 30f;
-            Debug.Log("now jumpdegree : {_jumpdegree}");
+            StartCoroutine("buttonPressSec");
         }
-        else if (Input.GetKeyUp(KeyCode.Space))
+        else if(Input.GetKeyUp(KeyCode.Space) && _isJumping == false)
         {
-            _rigidbody.AddForce(Vector3.up * _jumpdegree);
+            StartCoroutine("Jump");
+        }    
+    }
+    IEnumerator buttonPressSec()
+    {
+        while(_isJumping==false && _buttonPressedTime < 20f)
+        {
+            yield return new WaitForSeconds(0.03f);
+            _buttonPressedTime += 0.05f;
+        }
+    }
+    IEnumerator Jump()
+    {
+        _isJumping = true;
+        Vector3 pos = transform.position;
+        Debug.Log($"버튼 누른 시간: {_buttonPressedTime}");
+        while(pos.y < 10 && _buttonPressedTime > 0)
+        {
+            yield return new WaitForSeconds(0.002f);
+            pos.y += _buttonPressedTime * Time.deltaTime;
+            _buttonPressedTime -= 0.1f;
+            transform.position = pos;
+        }
+        _buttonPressedTime = 0.0f;
+        _isJumping = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            _isJumping = false;
         }
     }
 }
