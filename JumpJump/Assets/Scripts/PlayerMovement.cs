0319bfa6cc7fe6e49d91b0e672c07809f418e2d8
kwonheeyoung <heeyoung8748@gmail.com>
2022-07-25 13:02:49 +0900

    Feat: 제자리 점프
    아직 누른 시간만큼 높게 뛰지 못하고, 누를 때마다 일정 높이가 추가된다...

--- JumpJump/Assets/Scripts/PlayerMovement.cs
+++ JumpJump/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    private float _jumpdegree = 0.0f;
+    public int MoveSpeed = 3;
+    public float _jumpdegree = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        
-    }
 
+
+    }
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal"); 
+        float v = Input.GetAxis("Vertical");
+    }
     // Update is called once per frame
     void Update()
     {
-        Jump();    
-    }
-
-    void Jump()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && _jumpdegree >= 3)
+        if (Input.GetKeyDown(KeyCode.Space) && _jumpdegree <= 250)
         {
-            _jumpdegree += 0.3f;
+            _jumpdegree += 30f;
+            Debug.Log("now jumpdegree : {_jumpdegree}");
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            _rigidbody.AddForce(Vector3.up * _jumpdegree, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * _jumpdegree);
         }
     }
 }
