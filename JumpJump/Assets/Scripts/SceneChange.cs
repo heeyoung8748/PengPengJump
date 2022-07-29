a3fa8297a94a949431be93a1f37f4ea8e357d136
kwonheeyoung <heeyoung8748@gmail.com>
2022-07-29 15:55:20 +0900

    Feat: 시작화면 글자 깜빡임

--- JumpJump/Assets/Scripts/SceneChange.cs
+++ JumpJump/Assets/Scripts/SceneChange.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneChange : MonoBehaviour
 {
-    
+    private float _elapsedTime;
+    private TextMeshProUGUI _startText;
+    private bool _isBlink;
+    private void Start()
+    {
+        _startText = GetComponentInChildren<TextMeshProUGUI>();
+        _isBlink = true;
+    }
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.L))
         {
             SceneManager.LoadScene("Play");
+        }
+        TextBlink();
+    }
+
+    void TextBlink()
+    {
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime >= 0.5f)
+        {
+            _elapsedTime = 0f;
+            _isBlink = !_isBlink;
+            return;
+        }
+        if(_isBlink)
+        {
+            _startText.text = "Press L to start";
+        }
+        else
+        {
+            _startText.text = " ";
         }
     }
 }
