8714d1b40b2827219dab493b91778bafbfbfb4c0
heeyoung <heeyoung8748@gmail.com>
2022-08-03 02:37:51 +0900

    Feat: 점프 중 회전
    조작감이 매우 안좋음
    점프 속도 및 높이 조정 필요
    안 쓰는 코드 삭제 필요

--- JumpJump/Assets/Scripts/Player/PlayerMovement.cs
+++ JumpJump/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     //private float _maxJumpDegree = 50f;
     private float _buttonPressedTime = 0.0f;
     private bool _isJumping = false;
+    private bool _isReadyToJump = false;
     private GameObject _ground;
 
     // Start is called before the first frame update
@@ -21,44 +22,72 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if(_isJumping == false)
         {
-            StartCoroutine("buttonPressSec");
-        }
-        else if(Input.GetKeyUp(KeyCode.Space) && _isJumping == false)
-        {
-            // StartCoroutine("PenguinTumble");
-            Jump();
+            if (Input.GetKey(KeyCode.Space) && _isReadyToJump == false)
+            {
+                _isReadyToJump = true;
+                StartCoroutine("buttonPressSec");
+            }
+            else if(Input.GetKeyUp(KeyCode.Space))
+            {
+                StartCoroutine("Tumble");
+                StartCoroutine("Jump");
+                _isReadyToJump = false;
+            }
         }
     }
 
     IEnumerator buttonPressSec()
     {
-        while(_isJumping==false)
+        while(_isReadyToJump)
         {
             yield return new WaitForSeconds(0.1f);
-            _buttonPressedTime += 0.8f;
+            _buttonPressedTime += 0.2f;
             Vector3 groundPos = _ground.transform.position;
-            groundPos.y -= 0.005f;
+            groundPos.y -= 0.1f;
             _ground.transform.position = groundPos;
 
         }
     }
 
-    
-    void Jump()
+
+    IEnumerator Jump()
     {
         if(_isJumping)
         {
-            return;
+            yield break;
         }
         _isJumping = true;
         Debug.Log($"버튼 누른 시간: {_buttonPressedTime}");
-        //_rigidbody.AddForce(0, 80f, _buttonPressedTime);
-        _rigidbody.AddForce(0, 40f * JumpSpeed, 0);
-        _rigidbody.AddForce(transform.forward * _buttonPressedTime * JumpSpeed * Time.deltaTime);
-
+        _rigidbody.AddForce(0, 600f, 0);
+        while(_buttonPressedTime > 0)
+        {
+            _rigidbody.AddForce(transform.forward * _buttonPressedTime * JumpSpeed * Time.deltaTime);
+            _buttonPressedTime -= Time.deltaTime;
+        }
         _buttonPressedTime = 0.0f;
+    }
+
+    private float _deltaAngle = 360 / 4; // 점프 시간 동안 360도를 회전해야 한다.
+    private float _elapsedTime = 0f;
+    private Vector3 _initPosition;
+    IEnumerator Tumble()
+    {
+        _elapsedTime = 0f;
+        _initPosition = transform.forward;
+
+        while (_elapsedTime < 4)
+        {
+            _elapsedTime += Time.deltaTime;
+
+            //Vector3 deltaPosition = new Vector3(0f, 40f * Mathf.Sin(_elapsedTime), _elapsedTime * _buttonPressedTime);
+            //_rigidbody.MovePosition(_initPosition + deltaPosition);
+            Quaternion deltaQuaternion = Quaternion.Euler(_deltaAngle * Time.deltaTime, 0f, 0f);
+            _rigidbody.MoveRotation(_rigidbody.rotation * deltaQuaternion);
+
+            yield return null;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -67,10 +96,14 @@ public class PlayerMovement : MonoBehaviour
         {
             Die();
         }
-        else
+        else if(collision.transform.tag == "Ground")
         {
             _isJumping = false;
             _ground = collision.gameObject;
+        }
+        else
+        {
+            Die();
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -80,9 +113,9 @@ public class PlayerMovement : MonoBehaviour
             Vector3 lookPos = other.transform.position;
             lookPos.y = 0;
             transform.LookAt(lookPos);
+
         }
     }
-
     void Die()
     {
         gameObject.SetActive(false);
